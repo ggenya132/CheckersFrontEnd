d0215676dffce226e4cc19902b3bc1bf371da7da
Eugene Vendensky <eugenevendensky@zipcoders-MacBook-Pro-3.local>
2017-03-06 16:08:21 -0500

    first commit

---
+++ src/pages/game-board/game-board.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the GameBoard page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-game-board',
+  templateUrl: 'game-board.html'
+})
+export class GameBoardPage {
+
+  gameState : any = []; 
+ 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.gameState = {
+
+      
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GameBoardPage');
+  }
+
+}
